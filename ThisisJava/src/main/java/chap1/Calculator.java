d1cd075a3baeea14df6bd273066ec906e2d0c2a0
hgy <gyha0124@gmail.com>
2025-07-24 19:36:10 +0900

    250724

---
+++ ThisisJava/src/main/java/chap1/Calculator.java
@@ -0,0 +1,10 @@
+package chap1;
+
+public class Calculator {
+    public static void main(String[] args){
+        int x = 1;                        //변수 x 를 선언하고 1을 저장
+        int y = 2;                        // 변수 y 를 선언하고 2를 저장
+        int result = x + y;               // 변수 result를 선언하고 x와 y를 더한 값을 저장
+        System.out.println(result);       // 콘솔에 출력하는 메소드 호출
+    }
+}
